4da6dfcb411edc8cc6a9911d4ac3d1bdb8789159
mes32 <mes32@users.noreply.github.com>
2016-12-22 23:42:15 -0500

    Initial commit

---
+++ TicTacToeGame.java
@@ -0,0 +1,13 @@
+/*
+    A game of tic-tac-toe in Java.
+
+    Let's move quickly over difficult terrain.
+*/
+
+import java.io.*;
+
+class TicTacToeGame {
+    public static void main(String[] args) {
+        System.out.println("Hello tic-tac-toe!");
+    }
+}
